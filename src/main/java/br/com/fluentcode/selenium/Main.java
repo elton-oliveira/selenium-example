48040531b3f72853957b1ed90adfc5838bfe088e
elton-oliveira <elton.pmp@gmail.com>
2014-06-04 22:13:01 -0300

    Code format

--- src/main/java/br/com/fluentcode/selenium/Main.java
+++ src/main/java/br/com/fluentcode/selenium/Main.java
@@ -6,21 +6,21 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		// Go to the Google home page
 		driver.get("http://www.google.com.br/");
-		
-		// Enter de string 'Selenium'
-        WebElement campoDeTexto = driver.findElement(By.name("q"));
-        campoDeTexto.sendKeys("Selenium");
 
-        // Submit the form
-        campoDeTexto.submit();
-         
-        driver.quit();
+		// Enter de string 'Selenium'
+		WebElement campoDeTexto = driver.findElement(By.name("q"));
+		campoDeTexto.sendKeys("Selenium");
+
+		// Submit the form
+		campoDeTexto.submit();
+
+		driver.quit();
 	}
 
 }
